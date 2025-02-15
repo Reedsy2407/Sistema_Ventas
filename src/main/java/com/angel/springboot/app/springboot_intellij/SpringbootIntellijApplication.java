85b92d5f732dfcbf36bb3f6a3f5dff0776dd3b0e
Reedsy2407 <caraplomobaneado@gmail.com>
2025-02-15 04:25:31 -0500

    Cambios para el despliegue

--- src/main/java/com/angel/springboot/app/springboot_intellij/SpringbootIntellijApplication.java
+++ src/main/java/com/angel/springboot/app/springboot_intellij/SpringbootIntellijApplication.java
@@ -2,13 +2,18 @@ package com.angel.springboot.app.springboot_intellij;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootIntellijApplication {
+public class SpringbootIntellijApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(SpringbootIntellijApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootIntellijApplication.class);
+	}
 }
